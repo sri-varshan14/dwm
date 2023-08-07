5e250e5b7838249599472ad0f131381145e7afea
sri-varshan14 <mailmeatsri14@gmail.com>
2023-08-07 10:48:42 +0000

    Added systray patch and i3 layout patch

--- config.h
+++ config.h
@@ -12,6 +12,12 @@ static const char mocha_base[]       = "#1e1e2e";
 static const char mocha_crust[]       = "#11111b";
 static const char mocha_text[]       = "#cdd6f4";
 
+static const unsigned int systraypinning = 0;   /* 0: sloppy systray follows selected monitor, >0: pin systray to monitor X */
+static const unsigned int systrayonleft = 0;    /* 0: systray in the right corner, >0: systray on left of status text */
+static const unsigned int systrayspacing = 2;   /* systray spacing */
+static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display systray on the first monitor, False: display systray on the last monitor*/
+static const int showsystray        = 1;        /* 0 means no systray */
+
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { mocha_text, mocha_base, mocha_base },
